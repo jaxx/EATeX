788aaa0149e061d4656aa396dd43237b1705517b
Jakko Sikkar <jakko.sikkar@gmail.com>
2015-06-19 14:18:08 +0300

    initial commit

---
+++ register_addin.bat
@@ -0,0 +1,1 @@
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\regasm C:\eatex\src\EATeX\bin\Debug\EATeX.dll /codebase
---
+++ src/EATeX/EATeXAddin.cs
@@ -0,0 +1,42 @@
+﻿using EA;
+
+namespace EATeX
+{
+    public class EATeXAddin : EAAddinBase
+    {
+        public override object EA_GetMenuItems(Repository Repository, string MenuLocation, string MenuName)
+        {
+            switch (MenuName)
+            {
+                case "":
+                    return AddinMenu.Name;
+                case AddinMenu.Name:
+                    return new[]
+                    {
+                        AddinMenu.SubItems.GeneratePdf,
+                        AddinMenu.MenuSeparator,
+                        AddinMenu.SubItems.EditTemplate,
+                        AddinMenu.SubItems.Settings,
+                        AddinMenu.MenuSeparator,
+                        AddinMenu.SubItems.About
+                    };
+            }
+
+            return "";
+        }
+    }
+
+    public static class AddinMenu
+    {
+        public const string Name = "-&EATeX";
+        public const string MenuSeparator = "-";
+
+        public static class SubItems
+        {
+            public const string GeneratePdf = "&Generate PDF document";
+            public const string EditTemplate = "&Edit LaTeX template";
+            public const string Settings = "&Settings";
+            public const string About = "&About";
+        }
+    }
+}
