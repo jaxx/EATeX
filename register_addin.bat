2f48c786a9b4fd1f1c9e9f87bb57be3871e7d5d6
Jakko Sikkar <jakko.sikkar@gmail.com>
2015-06-22 18:29:26 +0300

    these scripts are not needed anymore (setup replaces those)

--- register_addin.bat
+++
@@ -1,2 +0,0 @@
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\regasm C:\eatex\src\EATeX\bin\Debug\EATeX.dll /codebase
-rem %WINDIR%\Microsoft.NET\Framework\v4.0.30319\regasm C:\eatex\src\EATeX\bin\Debug\EATeX.dll /u
